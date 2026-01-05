3b0e94660222dbf9bade5b200935434133c89015
Linqi Liu <liulinq@shernet.sheridancollege.ca>
2026-01-05 11:17:30 -0500

    Learnt how to move a shape, follow a mouse, and keep a shape on the screen

---
+++ Assets/Scripts/FirstScript.cs
@@ -0,0 +1,25 @@
+                                                                                                                                                                using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    float speed = 0.01f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPosition = transform.position;
+        newPosition.x += speed;
+        transform.position = newPosition;
+
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        if(screenPos.x < -10 || screenPos.x > Screen.width)
+        {
+            speed *= -1;
+        }
+    }
+}
